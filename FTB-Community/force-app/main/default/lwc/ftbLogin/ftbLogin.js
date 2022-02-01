7b90e552af7dbf9be1dfdf075eb7290c5e3758a4
francescopanico <frpanico96@gmail.com>
2022-02-01 22:08:35 +0100

    [FTB - Login] Header first implementation

--- FTB-Community/force-app/main/default/lwc/ftbLogin/ftbLogin.js
+++ FTB-Community/force-app/main/default/lwc/ftbLogin/ftbLogin.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
-export default class FtbLogin extends LightningElement {}
+export default class FtbLogin extends LightningElement 
+{
+    /* @api fields */
+    /* @track fields */
+    @track loginMessage;
+
+    /* connected callback */
+    connectedCallback()
+    {
+        /* get login message from configuration
+        * 01/02/2022 mock
+        */
+       this.loginMessage = 'FTB - La prima community di Fantabasket italiana';
+    }
+}
---
+++ FTB-Community/force-app/main/default/lwc/ftbLoginHeader/ftbLoginHeader.js
@@ -0,0 +1,8 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class FtbLoginHeader extends LightningElement 
+{
+    /* @api fields */
+    @api loginMessage
+
+}
