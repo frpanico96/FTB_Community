72cfc6327c984324c2ec26a2cae3cf01bdce6e40
francescopanico <frpanico96@gmail.com>
2022-02-06 13:03:54 +0100

    [FTB-APP] App v0.1.1

---
+++ FTB-Community/force-app/main/default/lwc/ftbHeader/ftbHeader.js
@@ -0,0 +1,11 @@
+/**@frapnico 2022-02-06
+ * FTB
+ * Generic Header for Components
+ * Message is retrieved from Custom Metadata
+ */
+import { LightningElement } from 'lwc';
+import FtbUtils from '../ftbUtils/ftbUtils';
+
+export default class FtbHeader extends LightningElement {
+    
+}
--- FTB-Community/force-app/main/default/lwc/ftbLoginHeader/ftbLoginHeader.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, api, track } from 'lwc';
-
-export default class FtbLoginHeader extends LightningElement 
-{
-    /* @api fields */
-    @api loginMessage
-
-}
