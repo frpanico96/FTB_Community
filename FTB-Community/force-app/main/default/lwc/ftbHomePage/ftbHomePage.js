3c8a8d86fa01290eb47f64490d2abab53bca7871
francescopanico <frpanico96@gmail.com>
2022-10-06 22:58:18 +0200

    [FTB] homepage v0.1 - navbar

--- FTB-Community/force-app/main/default/lwc/ftbHomePage/ftbHomePage.js
+++ FTB-Community/force-app/main/default/lwc/ftbHomePage/ftbHomePage.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
-
-export default class FtbHomePage extends LightningElement {}
+/**@frpanico 2022-06-07
+ * FTB
+ * Home Page Component
+ */
+import { LightningElement, api, track } from 'lwc';
+/* Import Utils Component */
+import FtbUtils from 'c/ftbUtils';
+export default class FtbHomePage extends FtbUtils
+{
+  @track navObj =
+  [
+    {name: 'home', label: 'Home'},
+    {name: 'teams', label: 'Teams'},
+    {name: 'standings', label: 'Standings'},
+    {name: 'trades', label: 'Trades'},
+  ]
+}
