13ea105ac5c8a224b93620c011b2110b40c98fa8
francescopanico <frpanico96@gmail.com>
2022-10-17 23:05:51 +0200

    [FTB] HomePage - team table v0.1

---
+++ FTB-Community/force-app/main/default/lwc/ftbAccordion/ftbAccordion.js
@@ -0,0 +1,95 @@
+import { LightningElement, track, api } from 'lwc';
+
+export default class FtbAccordion extends LightningElement 
+{
+  @api accordionItems = 
+  [
+    {
+      id: 1,
+      teamName: 'Team Test 1',
+      header: 
+      [
+       {
+        label: 'Role',
+        fieldName: 'role',
+        hideLabel: true
+       },
+       {
+        label: 'Full Name',
+        fieldName: 'fullname',
+        hideLabel: true
+       },
+       {
+        label: 'Points (Avg)',
+        fieldName: 'avgpoints',
+        hideLabel: true
+       }
+      ],
+      dataObj:
+      [
+        {
+          id: '1',
+          role: 'G',
+          fullname: 'test1',
+          avgpoints: 32
+        },
+        {
+          id: '2',
+          role: 'C',
+          fullname: 'test2',
+          avgpoints: 25
+        },
+        {
+          id: '3',
+          role: 'F',
+          fullname: 'test3',
+          avgpoints: 28
+        }
+      ]
+    },
+    {
+      id: 2,
+      teamName: 'Team Test 2',
+      header: 
+      [
+       {
+        label: 'Role',
+        fieldName: 'role',
+        hideLabel: true
+       },
+       {
+        label: 'Full Name',
+        fieldName: 'fullname',
+        hideLabel: true
+       },
+       {
+        label: 'Points (Avg)',
+        fieldName: 'avgpoints',
+        hideLabel: true
+       }
+      ],
+      dataObj:
+      [
+        {
+          id: '4',
+          role: 'G',
+          fullname: 'test4',
+          avgpoints: 19
+        },
+        {
+          id: '5',
+          role: 'C',
+          fullname: 'test5',
+          avgpoints: 10
+        },
+        {
+          id: '6',
+          role: 'F',
+          fullname: 'test6',
+          avgpoints: 31
+        }
+      ]
+    }
+  ];
+
+}
---
+++ FTB-Community/force-app/main/default/lwc/ftbDataTable/ftbDataTable.js
@@ -0,0 +1,14 @@
+import { LightningElement, track, api } from 'lwc';
+
+export default class FtbDataTable extends LightningElement 
+{
+  @api tableData = [];
+  @api tableColumns = [];
+  @api tableHeader;
+
+  connectedCallback()
+  {
+    console.log('TableData >>> ' + JSON.stringify(this.tableData));
+    console.log('TableColumns >>> ' + JSON.stringify(this.tableColumns));
+  }
+}
