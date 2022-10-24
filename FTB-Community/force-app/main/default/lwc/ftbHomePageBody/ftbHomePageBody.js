a19e1f3889e3228cdd77b1633cb568d5c68985dd
francescopanico <frpanico96@gmail.com>
2022-10-24 22:21:22 +0200

    [FTB] HomePage - Components destructuring

---
+++ FTB-Community/force-app/main/default/lwc/ftbHomePageBody/ftbHomePageBody.js
@@ -0,0 +1,6 @@
+import { LightningElement, api } from 'lwc';
+
+export default class FtbHomePageBody extends LightningElement 
+{
+  @api homePage = false;
+}
---
+++ FTB-Community/force-app/main/default/lwc/ftbHomePageHome/ftbHomePageHome.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class FtbHomePageHome extends LightningElement {}
