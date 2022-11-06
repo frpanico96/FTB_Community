bc7ee2bd67dafebff69d6913344b4ddab7500ec5
francescopanico <frpanico96@gmail.com>
2022-11-06 17:22:18 +0100

    [FTB] HomePage - Fetch data from server v0.2

--- FTB-Community/force-app/main/default/lwc/ftbHomePageBody/ftbHomePageBody.js
+++ FTB-Community/force-app/main/default/lwc/ftbHomePageBody/ftbHomePageBody.js
@@ -1,6 +1,8 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
 
 import cometDsubscription from '@salesforce/apex/FTB_LC_Utils.handleCometdSubscription';
+import fetchData from '@salesforce/apex/FTB_LC_HomePageHome.fetchData';
 
 /* Import Utils Component */
 import FtbUtils from 'c/ftbUtils';
@@ -9,10 +11,29 @@ export default class FtbHomePageBody extends FtbUtils
 {
   @api homePage = false;
 
+  @track championshipColumn = [];
+  @track championshipData = [];
+  @track teamColumns = [];
+  @track teamData = [];
+
+  @wire(CurrentPageReference)
+  currentPageParameters(currentPageReference)
+  {
+    if(currentPageReference)
+    {
+      console.log(currentPageReference);
+      if(!currentPageReference.state) return;
+      setTimeout(() => {
+        this.handleDataFetch(currentPageReference.state['payload'])
+      }, 3000);
+    }
+  }
+
   connectedCallback()
   {
     console.log('@@@ Starting Connection');
     this.handleCometDSubscription();
+
   }
 
   handleCometDSubscription()
@@ -31,9 +52,44 @@ export default class FtbHomePageBody extends FtbUtils
       })
   }
 
-  handleSubscribeEvent()
+  handleDataFetch(sessionId)
   {
-    console.log('@@@ Home Subscribed');
+    console.log('@@@ SessionId >>> ' + sessionId);
+    fetchData({ sessionId: sessionId})
+    .then(() => 
+    {
+      console.log('@@@ Start Fetching');
+    })
+    .catch(error => 
+    {
+        console.log('@@@ Error Fetching >>> ' + JSON.stringify(error));
+        this.showMessage('Error', error, 'error');
+    });
+  }
+
+  handleSubscribeEvent(event)
+  {
+    console.log('@@@ Home Subscribed >>> ' + JSON.stringify(event));
+    let response = JSON.parse(event.detail.data.payload.FTB_SerializedMessage__c);
+    console.log(response.SUCCESS);
+    console.log(response.ERROR_MESSAGE);
+    console.log(response.ERROR_DESCRIPTION);
+    console.log(response.ERROR_CODE);
+    console.log(response.IDENTIFICATION_KEY);
+    if(response.SUCCESS)
+    {
+      let payload = JSON.parse(response.ERROR_DESCRIPTION);
+      this.championshipColumn = payload.championshipColumns;
+      this.teamColumns = payload.teamColumns;
+
+      const teamObj = payload.teamsData;
+      for(let singleTeam of teamObj)
+      {
+        Object.defineProperty(singleTeam, '_children', Object.getOwnPropertyDescriptor(singleTeam, 'children'));
+      }
+      console.log('@@@ After Renaming >>> ' + JSON.stringify(teamObj));
+      this.championshipTable = payload.championshipData[0];
+    }
   }
 
 
