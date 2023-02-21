8a1944a93988685a3ba548816f0de7c02de7399e
francescopanico <frpanico96@gmail.com>
2023-02-21 23:17:22 +0100

    [FTB] Team - Playcourt first implementation General UI first implementation

--- FTB-Community/force-app/main/default/lwc/ftbHomePageTeam/ftbHomePageTeam.js
+++ FTB-Community/force-app/main/default/lwc/ftbHomePageTeam/ftbHomePageTeam.js
@@ -1,3 +1,53 @@
-import { LightningElement } from 'lwc';
+/* @frpanico
+* FTB Component for the Teams Page
+*/
+import { LightningElement, api, track } from 'lwc';
 
-export default class FtbHomePageTeam extends LightningElement {}
+import FtbUtils from 'c/ftbUtils';
+
+export default class FtbHomePageTeam extends FtbUtils
+{
+
+  @track fetchRequest = false;
+
+  @track placeholderCombo = 'Select a Team...';
+  @track optionTeams =
+  [
+    {label: 'Team 1', value: 'team1'},
+    {label: 'Team 2', value: 'team2'},
+    {label: 'Team 3', value: 'team3'},
+  ];
+  @track currentTeam = 'team1';
+  
+  connectedCallback()
+  {
+    console.log('### Team Connecting...');
+    this.dispatchEvent(new CustomEvent('connecting', {detail: this.constantsObj.TEAMS}));
+  }
+  disconnectedCallback()
+  {
+    console.log('### Team Disconnecting...');
+    this.dispatchEvent(new CustomEvent('disconnecting', {detail: this.constantsObj.TEAMS}));
+    
+  }
+  @api 
+  homeInitialization(sessionId)
+  {
+    if(!sessionId) return;
+    if(this.fetchRequest) return;
+
+    this.fetchRequest = true;
+
+    console.log('### Fetching Team...');
+  }
+  @api
+  homeConfiguration(event)
+  {
+    console.log('### Configurating Team...');
+  }
+
+  handleTeamChange(event)
+  {
+    console.log('### Changing Team...');
+  }
+}
